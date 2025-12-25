072054940796bf1786a344ecc5cd5b4307e5d63a
CRBroughton <crbroughton@posteo.uk>
2025-12-25 14:54:56 +0000

    feat(funcs): :test_tube: add failing test for the builtin add method

---
+++ funcs_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestBuildinAdd(t *testing.T) {
+	env := NewEnv(nil)
+	env.Define("+", makeBuiltin(builtinAdd))
+
+	tests := []struct {
+		input string
+		want  int
+	}{
+		{"(+ 1 2)", 3},
+		{"(+ 1 2 3)", 6},
+		{"(+ 1 2 3 4 5)", 15},
+		{"(+ 0)", 0},
+		{"(+ -5 5)", 0},
+		{"(+ 100 -50)", 50},
+	}
+
+	for _, tt := range tests {
+		expr := readStr(tt.input)
+		result := eval(expr, env)
+
+		if result.Type != Number {
+			t.Errorf("%s: type = %v, want Number", tt.input, result.Type)
+		}
+		if result.Num != tt.want {
+			t.Errorf("%s = %d, want %d", tt.input, result.Num, tt.want)
+		}
+
+	}
+}
