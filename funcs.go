4bfbe22cf4319578d23de76cc5e423652f44c5cc
CRBroughton <crbroughton@posteo.uk>
2025-12-25 15:10:57 +0000

    feat(funcs): :white_check_mark: passing test for builtinSub

--- funcs.go
+++ funcs.go
@@ -7,3 +7,14 @@ func builtinAdd(args []*Expr) *Expr {
 	}
 	return makeNum(sum)
 }
+
+func builtinSub(args []*Expr) *Expr {
+	if len(args) == 0 {
+		return makeNum(0)
+	}
+	result := args[0].Num
+	for i := 1; i < len(args); i++ {
+		result -= args[i].Num
+	}
+	return makeNum(result)
+}
